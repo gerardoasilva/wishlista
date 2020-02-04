e63ccb277a819fd5ff903a43da340866a00792e5
Gerardo Silva <gsilva.razo@gmail.com>
2020-02-03 19:40:05 -0600

    DB config

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// mongodb://localhost/wishlista
-// mongodb+srv://wishlista:wishlistainvierno20@cluster0-xilgz.mongodb.net/wishlista?retryWrites=true&w=majority
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://wishlista:wishlistainvierno20@cluster0-xilgz.mongodb.net/wishlista?retryWrites=true&w=majority";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/wishlista¨
+// exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://wishlista:wishlistainvierno20@cluster0-xilgz.mongodb.net/wishlista?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
